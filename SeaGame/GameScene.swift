29a8ca3ab85229b851ea9fb6ce0221b0cd068626
P7ngu <60442992+P7ngu@users.noreply.github.com>
2023-12-07 11:08:41 +0100

    removed useless variables

--- SeaGame/GameScene.swift
+++ SeaGame/GameScene.swift
@@ -10,13 +10,8 @@ import GameplayKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate { //We add the contact delegate to dected collisions and send notifications when they happen, hence react to them happening
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
     private var lastUpdateTime : TimeInterval = 0
   
-    private var spinnyNode : SKShapeNode?
-    
     //Custom vars:
     let player = SKSpriteNode(imageNamed: "player-submarine")
     var touchingPlayer = false
